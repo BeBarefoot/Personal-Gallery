3cad0af2c5f22c042380b8a06bbd8f3fc87e267e
bebarefoot <35373216+BeBarefoot@users.noreply.github.com>
2018-10-13 19:47:21 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,69 @@
-console.log('Starting up');
-
 function initPage() {
     renderProj()
+    renderModals()
 }
 
 function renderProj() {
     var projects = getProj()
-    console.log(projects);
-    
-    elModalBody = projects.map((project)=>{return (
-        `
-    <h2>${project.name}</h2>
-    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-    <img class="img-fluid d-block mx-auto" src="img/portfolio/${project.img}" alt="">
-    <p>${project.desc}</p>
-    <ul class="list-inline">
-      <li>Date: ${project.publishedAt}</li>
-      <li>Client: Threads</li>
-      <li>Category: ${project.cat}</li>
-      <li>Link: <a  href=${project.url}>Project</a></li>
-    </ul>
-    <button class="btn btn-primary" onclick="showModal(${project.id})" type="button">
-        <i class="fa fa-times"></i>
-        Close Project</button>
+    elModalBody = projects.map((project) => {
+        return (
+            `
+             <div class="col-md-4 col-sm-6 portfolio-item">
+                    <a id="modal${project.id}" class="portfolio-link" onclick="showModal(${project.id})" data-toggle="modal${project.id}" href="#portfolioModal${project.id}">
+                        <div class="portfolio-hover">
+                            <div class="portfolio-hover-content">
+                                <i class="fa fa-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="img/portfolio/${project.img}" alt="">
+                    </a>
+                    <div class="portfolio-caption">
+                        <h4>${project.title}</h4>
+                        <p class="text-muted">${project.desc}</p>
+                    </div>
+                </div>
         `)
     })
-    $('.modal-body').html(elModalBody.join(''))
+    $('.project-body').html(elModalBody.join(''))
+}
 
-
-
+function renderModals() {
+    var projects = getProj()
+    elModalBody = projects.map((project) => {
+        return (
+            `
+ <div class="portfolio-modal modal fade" id="portfolioModal${project.id}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
+                    <div class="lr">
+                        <div class="rl"></div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 mx-auto">
+                            <div class="modal-body">
+                                                <h2>${project.name}</h2>
+                                <p class="item-intro text-muted">${project.title}</p>
+                                <img class="img-fluid d-block mx-auto" src="img/portfolio/${project.img}" alt="">
+                                <p>${project.desc}</p>
+                                <ul class="list-inline">
+                                    <li>Date: ${project.date}</li>
+                                    <li>Category: ${project.cat}</li>
+                                    <li>Link: <a href="${project.url}" target="_blank">Github</a> </li>
+                                </ul>
+                                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                  <i class="fa fa-times"></i>
+                  Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        `)
+    })
+    $('.modal-section').html(elModalBody.join(''))
 }
--- js/proj-service.js
+++ js/proj-service.js
@@ -1,28 +1,53 @@
 var gIdx = 1
-var gProjs = [
-    {id:gIdx++,
-    name:'Book Store',
-    title:'Just A Bookstore!',
-    desc:'An interctive App for managing a bookstore',
-    url:'../Day 20 - Book Store/index.html',
-    cat:'App',
-    img:'bookstore.jpg'
+var gProjs = [{
+        id: gIdx++,
+        name: 'Book Store',
+        title: 'Just A Bookstore!',
+        desc: 'An interctive App for managing a bookstore',
+        url: 'https://github.com/BeBarefoot/Book-Store',
+        cat: 'App',
+        img: 'bookstore.jpg',
+        date: 'September 2018'
     },
-    {id:gIdx++,
-    name:'x and o',
-    title:'X and O Game!',
-    desc:'Best X and O game ever maybe',
-    url:'https://github.com/BeBarefoot/X-O',
-    cat:'App',
-    img:'xo.png'
+    {
+        id: gIdx++,
+        name: 'x and o',
+        title: 'X and O Game!',
+        desc: 'Best X and O game ever maybe',
+        url: 'https://github.com/BeBarefoot/X-O',
+        cat: 'App',
+        img: 'xo.png',
+        date: 'June 2018'
     },
-    {id:gIdx++,
-    name:'Book Store',
-    title:'Just A Bookstore!',
-    desc:'An interctive App for managing a bookstore',
-    url:'../Day 20 - Book Store/index.html',
-    cat:'App',
-    img:''
+    {
+        id: gIdx++,
+        name: 'Guess Who',
+        title: 'Guess Who game',
+        desc: 'An interctive learning "Guess Who" game',
+        url: 'https://github.com/BeBarefoot/Guess-Me-Game',
+        cat: 'Game',
+        img: 'guess.jpg',
+        date: 'October 2018'
+    },
+    {
+        id: gIdx++,
+        name: 'To Do List',
+        title: 'Manage Your Stuff',
+        desc: `An online list managment experience like nothing you've ever experienced`,
+        url: 'https://github.com/BeBarefoot/To-Do-List-Maker',
+        cat: 'App',
+        img: 'todo.jpg',
+        date: 'October 2018'
+    },
+    {
+        id: gIdx++,
+        name: 'Touch The Numbers',
+        title: 'Touch Some Numbers',
+        desc: `If you're gonna touch somthing - touch numbers!`,
+        url: 'https://github.com/BeBarefoot/To-Do-List-Maker',
+        cat: 'Game',
+        img: 'touch.jpg',
+        date: 'September 2018'
     },
 ]
 
@@ -32,10 +57,7 @@ function getProj() {
     else return gProjs = JSON.parse(localStorage.getItem('projs'))
 }
 
-function createProj() {
-
-}
 
 function showModal(idx) {
-    // $('#modal'idx).modal('show')
+    $('#portfolioModal' + idx).modal('show')
 }
